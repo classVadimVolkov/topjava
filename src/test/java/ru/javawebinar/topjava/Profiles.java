83c73833061bdfd2fcb1383dbfcc9d97e0eb60a0
Vadim Volkov <antibiotik15@ya.ru>
2021-03-09 17:38:42 +0300

    apply 5_7_spring_data_jpa.patch

--- src/test/java/ru/javawebinar/topjava/Profiles.java
+++ src/test/java/ru/javawebinar/topjava/Profiles.java
@@ -7,11 +7,15 @@ import org.springframework.util.ClassUtils;
 public class Profiles {
     public static final String
             JDBC = "jdbc",
+            JPA = "jpa",
+            DATAJPA = "datajpa";
 
-    POSTGRES_DB = "postgres",
+    public static final String REPOSITORY_IMPLEMENTATION = DATAJPA;
+
+    public static final String
+            POSTGRES_DB = "postgres",
             HSQL_DB = "hsqldb";
 
-    public static final String ACTIVE_DB = POSTGRES_DB;
     //  Get DB profile depending of DB driver in classpath
     public static String getActiveDbProfile() {
         if (ClassUtils.isPresent("org.postgresql.Driver", null)) {
@@ -26,7 +30,8 @@ public class Profiles {
     //http://stackoverflow.com/questions/23871255/spring-profiles-simple-example-of-activeprofilesresolver
     public static class ActiveDbProfileResolver implements ActiveProfilesResolver {
         @Override
-        public @NonNull String[] resolve(@NonNull Class<?> aClass) {
+        public @NonNull
+        String[] resolve(@NonNull Class<?> aClass) {
             return new String[]{getActiveDbProfile()};
         }
     }
